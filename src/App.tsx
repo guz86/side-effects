77ca7ab4bf238933db16371678dce00c0cdf2a8f
Pavel Gordienko <guzmo@yandex.ru>
2024-12-15 07:33:57 +0300

    fix: path Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { Login } from './assets/Login/Login';
+import { Login } from './Login/Login';
 
 const App: React.FC = () => {
   return (
