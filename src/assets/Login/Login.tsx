66662a7c0170fba4143098907a722a43eb519569
Pavel Gordienko <guzmo@yandex.ru>
2024-12-15 05:35:36 +0300

    feat: css module

--- src/assets/Login/Login.tsx
+++ src/assets/Login/Login.tsx
@@ -1,24 +1,41 @@
-import './Login.css';
 import { FormEvent, useState } from 'react';
+import styles from './Login.module.css';
 
 export const Login = () => {
-  function submitHandler(event: FormEvent<HTMLFormElement>): void {
+  const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-  }
+    console.log('Form submitted');
+  };
 
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-          <label htmlFor="email">Password</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </>
+    <form onSubmit={submitHandler} className={styles.form}>
+      <div className={styles.div}>
+        <label htmlFor="email" className={styles.label}>
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className={styles.input}
+          required
+        />
+      </div>
+      <div className={styles.div}>
+        <label htmlFor="password" className={styles.label}>
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className={styles.input}
+          required
+        />
+      </div>
+      <button type="submit" className={styles.button}>
+        Login
+      </button>
+    </form>
   );
 };
