4a24caa7d3b7e47f237e89118bad53e682f81826
Pavel Gordienko <guzmo@yandex.ru>
2024-12-15 06:42:03 +0300

    feat: simple format validation

--- src/assets/Login/Login.tsx
+++ src/assets/Login/Login.tsx
@@ -1,15 +1,41 @@
-import { FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Login.module.css';
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [emailIsValid, setEmailIsValid] = useState<boolean>(true);
+  const [passwordIsValid, setPasswordIsValid] = useState<boolean>(true);
+
+  const [formatIsValid, setFormatIsValid] = useState(false);
+
   const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     console.log('Form submitted');
   };
 
+  function emailChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
+    setEmail(event.target.value);
+    setFormatIsValid(email.trim().includes('@') && password.trim().length > 5);
+  }
+
+  function passwordChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
+    setPassword(event.target.value);
+    setFormatIsValid(email.trim().includes('@') && password.trim().length > 5);
+  }
+
+  function validateEmailHandler() {
+    setEmailIsValid(email.trim().includes('@'));
+  }
+
+  function validatePasswordHandler() {
+    setPasswordIsValid(password.trim().length > 5);
+  }
+
   return (
     <form onSubmit={submitHandler} className={styles.form}>
-      <div className={styles.div}>
+      <div className={`${styles.div} ${!emailIsValid ? styles.invalid : ''}`}>
         <label htmlFor="email" className={styles.label}>
           Email
         </label>
@@ -18,10 +44,14 @@ export const Login = () => {
           id="email"
           name="email"
           className={styles.input}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
           required
         />
       </div>
-      <div className={styles.div}>
+      <div
+        className={`${styles.div} ${!passwordIsValid ? styles.invalid : ''}`}
+      >
         <label htmlFor="password" className={styles.label}>
           Password
         </label>
@@ -30,10 +60,12 @@ export const Login = () => {
           id="password"
           name="password"
           className={styles.input}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
           required
         />
       </div>
-      <button type="submit" className={styles.button}>
+      <button type="submit" disabled={!formatIsValid} className={styles.button}>
         Login
       </button>
     </form>
