54ab79f4505937e4a51a17e4fcba0a8fe9c789eb
Pavel Gordienko <guzmo@yandex.ru>
2024-12-15 07:28:46 +0300

    feat: use lodash for validate

--- src/assets/Login/Login.tsx
+++ src/assets/Login/Login.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Login.module.css';
+import _ from 'lodash';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
@@ -10,32 +11,56 @@ export const Login = () => {
 
   const [formatIsValid, setFormatIsValid] = useState(false);
 
+  const handleEmailChange = _.debounce((value: string) => {
+    console.log('handleEmailChange');
+    setEmail(value);
+    setFormatIsValid(value.trim().includes('@') && password.trim().length > 5);
+  }, 1000);
+
+  const handlePasswordChange = _.debounce((value: string) => {
+    console.log('handlePasswordChange');
+    setPassword(value);
+    setFormatIsValid(email.trim().includes('@') && value.trim().length > 5);
+  }, 1000);
+
+  //   useEffect(() => {
+  //     setFormatIsValid(email.trim().includes('@') && password.trim().length > 5);
+  //   }, [email, password]);
+
+  function emailChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
+    handleEmailChange(event.target.value);
+    // setEmail(event.target.value);
+    // setFormatIsValid(
+    //   event.target.value.trim().includes('@') && password.trim().length > 5
+    // );
+  }
+
+  function passwordChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
+    handlePasswordChange(event.target.value);
+    // setPassword(event.target.value);
+    // setFormatIsValid(
+    //   email.trim().includes('@') && event.target.value.trim().length > 5
+    // );
+  }
+
+  const validateEmailHandler = (event: ChangeEvent<HTMLInputElement>): void => {
+    const value = event.target.value.trim();
+    setEmail(value);
+    setEmailIsValid(value.includes('@'));
+  };
+
+  const validatePasswordHandler = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    const value = event.target.value.trim();
+    setPassword(value);
+    setPasswordIsValid(value.length > 5);
+  };
+
   const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     console.log('Form submitted');
   };
-
-  function emailChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
-    setEmail(event.target.value);
-    setFormatIsValid(
-      event.target.value.trim().includes('@') && password.trim().length > 5
-    );
-  }
-
-  function passwordChangeHandler(event: ChangeEvent<HTMLInputElement>): void {
-    setPassword(event.target.value);
-    setFormatIsValid(
-      email.trim().includes('@') && event.target.value.trim().length > 5
-    );
-  }
-
-  function validateEmailHandler() {
-    setEmailIsValid(email.trim().includes('@'));
-  }
-
-  function validatePasswordHandler() {
-    setPasswordIsValid(password.trim().length > 5);
-  }
 
   return (
     <form onSubmit={submitHandler} className={styles.form}>
